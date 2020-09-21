08f180f591c8d8a1e093972d3b80763d5cb735a3
dustin <dustinhitzing@gmail.com>
2020-09-20 21:02:41 -0400

    removed bad git init potentially :|

---
+++ CharacterManager/CharacterManager/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CharacterManager.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CharacterManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CharacterSheetMainPage();
+        }
+    }
+}
